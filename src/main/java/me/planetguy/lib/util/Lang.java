8012f7bd52c2080e4f311a2b5ee3e3e5960a92fc
planetguy32 <apricefrench2d@gmail.com>
2015-05-16 15:54:05 -0700

    Crash in debug settings only.

--- src/main/java/me/planetguy/lib/util/Lang.java
+++ src/main/java/me/planetguy/lib/util/Lang.java
@@ -1,12 +1,13 @@
 package me.planetguy.lib.util;
 
+import me.planetguy.lib.PlanetguyLib;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
 public class Lang {
 
 	public static String translate(String s) {
 		String res= LanguageRegistry.instance().getStringLocalization(s);
-		if(res.equals("")){
+		if(res.equals("") && PlanetguyLib.doPLLogging){ //this is a debug feature
 			throw new RuntimeException("Failed to translate "+s);
 		}else{
 			return res;
